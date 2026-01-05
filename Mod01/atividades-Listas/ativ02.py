9abd5ab5ca18979cc2e1e380fc615b10e3e82eb4
Bruna Lopes <brunadevs@yahoo.com>
2026-01-05 15:19:23 -0300

    Movi as pastas de listas, tuplas, sets e dicionários para o módulo 2.

--- Mod01/atividades-Listas/ativ02.py
+++
@@ -1,9 +0,0 @@
-"""
-    Faça a definição de uma lista contendo as vogais.
-    Finalmente, utilize o print() para exibir os valores da lista.
-"""
-
-# Lista de vogais
-vogais = ["a", "e", "i", "o", "u"]
-# Mostrando a lista
-print(vogais)
