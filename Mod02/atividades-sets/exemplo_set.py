d62bf23b0d60ebd2eaa20f3d7ab015617a07b7b0
Bruna Lopes <brunadevs@yahoo.com>
2026-01-08 09:04:05 -0300

    Entendendo sobre sets.

---
+++ Mod02/atividades-sets/exemplo_set.py
@@ -0,0 +1,25 @@
+# Estrutura de dados SETS
+'''
+🧠 Encontrar vs acessar (diferença importante!)
+
+🔎 Encontrar → verificar se algo existe (in)
+
+📍 Acessar por índice → pegar pela posição ([0])
+
+👉 Em sets, você encontra, mas não acessa por índice.
+
+🎈 Frase perfeita para prova ou explicação
+
+Sets não são ordenados e não possuem índices, portanto seus elementos não podem ser acessados ou modificados por posição.
+
+🧠 Resumão rápido
+
+Set ❌ não tem índice
+
+Set ❌ não acessa por posição
+
+Set ✅ verifica existência por valor
+
+Set ❌ não modifica elemento individual por índice
+'''
+
