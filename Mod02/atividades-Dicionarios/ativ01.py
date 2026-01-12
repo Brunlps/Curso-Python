2323014f839fb8724222156751db5e3660ebdae3
Bruna <brunadevs@yahoo.com>
2026-01-12 15:29:09 -0300

    Atividade concluida.

---
+++ Mod02/atividades-Dicionarios/ativ01.py
@@ -0,0 +1,11 @@
+'''Escreva um programa que EXIBA um dicionário contendo
+informações de pessoas (nome, idade) e exiba essas informações.'''
+
+# Criando um programa para mostra dados pessoais 
+# Usando dicionário para estruturar os dados
+
+cadastro_pessoa = {
+    'Nome': 'Bruna Kercia',
+    'Idade': 29
+}
+print(f'O nome: {cadastro_pessoa['Nome']} e idade {cadastro_pessoa['Idade']}')
