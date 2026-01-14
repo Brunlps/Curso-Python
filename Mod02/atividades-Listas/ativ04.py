2e10d81ee338bafa1888d1f666e3194887932ced
Bruna <brunadevs@yahoo.com>
2026-01-14 09:51:57 -0300

    Atividade concluida.

---
+++ Mod02/atividades-Listas/ativ04.py
@@ -0,0 +1,23 @@
+listas_feminino = ['Bruna', 'Janaina']
+listas_masculino = ['Jonas', 'joão']
+lista_mista = []
+
+
+print(listas_feminino)
+
+# Adicionando valores
+listas_feminino.append('Kercia')
+print(listas_feminino)
+
+# Ordenando listas
+listas_masculino.sort()
+print(listas_masculino)
+
+# Removendo valores
+# listas_feminino.remove('Jonas')
+# print(listas_feminino)
+
+# Unindo listas
+# listas_masculino.append(listas_feminino)
+# print(listas_masculino)
+
