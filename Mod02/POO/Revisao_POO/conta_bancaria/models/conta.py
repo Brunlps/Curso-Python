d56d0dd04375bfa60d93bf7800dc3e93608e86b7
Bruna Kércia <brunadevs@yahoo.com>
2026-04-01 15:36:21 -0300

    Task: Initial Bank Account Structure.

---
+++ Mod02/POO/Revisao_POO/conta_bancaria/models/conta.py
@@ -0,0 +1,6 @@
+class ContaBancaria:
+    def __init__(self, titular, saldo):
+        self.titular = titular
+        self.saldo = saldo
+        # self.saldo_retirado = float(self.saldo_retirado)
+        # self.valor_deposito = float(self.valor_deposito)
