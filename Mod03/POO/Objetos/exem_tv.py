11c0a58c77cf1852f831c7da71ed6b01f69e3e24
Bruna Kércia Lopes <seuemail@gmail.com>
2026-04-13 10:29:56 -0300

    exemplo do livro.

---
+++ Mod03/POO/Objetos/exem_tv.py
@@ -0,0 +1,19 @@
+# Classe principal
+class Televisao:
+    # construtor: onde cosntrimos os atributos da classe
+    def __init__(self):
+        self.ligada = False
+        self.canal = 2
+
+    # Métodos da classe
+    def mudar_canal_para_baixo(self):
+        self.canal -= 1
+
+    def mudar_canal_para_cima(self):
+        self.canal += 1
+        
+# Objeto que recebe a classe principal.
+tv = Televisao()
+tv.mudar_canal_para_baixo()
+tv.mudar_canal_para_cima()
+tv.canal
