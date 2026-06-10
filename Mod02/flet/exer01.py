27a40bbe22a9d51e7da34270da2dbdecead02f64
Bruna Kércia Lopes <seuemail@gmail.com>
2026-06-10 15:21:15 -0300

    flet: criado um módulo para estudo.

---
+++ Mod02/flet/exer01.py
@@ -0,0 +1,13 @@
+"""
+[PYIA-A09] Desenvolva uma aplicação utilizando o framework
+Flet que permite ao usuário adicionar itens a uma lista de
+tarefas. A interface do aplicativo deve incluir um campo de
+entrada de texto para o usuário digitar o nome da tarefa e
+um botão para adicionar a tarefa à lista. Quando o usuário
+clicar no botão, o item deverá ser adicionado a uma lista
+exibida na tela, mostrando todas as tarefas que foram
+incluídas até o momento. A lista de tarefas deve ser
+atualizada dinamicamente sempre que um novo item for
+adicionado.
+"""
+
