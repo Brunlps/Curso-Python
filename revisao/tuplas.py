417aaa0d879bf0ccb37225e8632ffc017589a3a8
Bruna Kércia Lopes <seuemail@gmail.com>
2026-07-01 16:16:14 -0300

    Revisão e desafio.

---
+++ revisao/tuplas.py
@@ -0,0 +1,7 @@
+# cria uma tupla com três
+# cores favoritas e tenta modificar
+# um valor dela. Me diz o que acontece!
+
+cores_favoritas = ("Vermelho", "Azul", "Branco", "Azul", "Branco")
+print(cores_favoritas.count("Azul"))
+print(cores_favoritas.index("Vermelho"))
